41ffb0e5319877d24edecb517dbe53c24e6ec58c
Nacim Goura <Nacim Goura>
2016-04-06 08:50:13 +0200

    Modification catégorie

--- client/views/categorie/newCategorie.js
+++ client/views/categorie/newCategorie.js
@@ -1,3 +1,29 @@
+Template.newCategorie.rendered = function() {
+    $('#colorpicker1').colorpicker({
+        customClass: 'colorpicker-2x',
+        sliders: {
+            saturation: {
+                maxLeft: 150,
+                maxTop: 150
+            },
+            hue: {
+                maxTop: 150
+            },
+            alpha: {
+                maxTop: 150
+            }
+        },
+        colorSelectors: {
+            'default': '#777777',
+            'primary': '#337ab7',
+            'success': '#5cb85c',
+            'info': '#5bc0de',
+            'warning': '#f0ad4e',
+            'danger': '#d9534f'
+        }
+    });
+};
+
 AutoForm.addHooks('newCategorie', {
     after: {
         insert: function(error) {
