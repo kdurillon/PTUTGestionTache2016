fecdb05b0c15540e9b40f8326fbbbbee35a429c6
lp <lp>
2016-04-06 17:20:48 +0200

    ok

---
+++ client/views/generationFormulaire/generer.js
@@ -0,0 +1,78 @@
+/**
+ * Created by lp on 06/04/2016.
+ */
+
+//************************************************************ chargement
+
+
+//************************************************************ événements
+
+
+Template.formulaireGenere.events({
+
+    "click #ajouterElmtFormGen" :function(){
+
+        var plus=$("#inputPlaceGenForm").val();
+        var html=div($("#inputTextGenForm").val(),$("#inputSelectGenForm").val(),plus);
+        if($("#inputTextGenForm").val()!="" && $("#inputPlaceGenForm").val()!="" ){$("#apercuForm").after(html);resetFormGenere();$('.datepicker').datepicker(); }
+
+    },
+    "click #ajouterTitreFormGen" :function() {
+    $("#apercuForm").html($("#inputTitreGenForm").val());
+    },
+    "change #inputSelectGenForm" : function(){
+        switch($("#inputSelectGenForm").val()){
+
+            case "1":{
+                $("#elemtPlusformGen").html(" <label for='inputPlaceGenForm' class='control-label'>Entrez l\'indication de saisie de votre élément :</label>"+
+               " <input id='inputPlaceGenForm' class='form-control' type='text'  placeholder='Votre question, le document demandé, le type de rendez-vous...'/>");
+                break;
+            }
+            case "2":{
+                $("#elemtPlusformGen").html("");
+                break;
+            }
+
+        }
+
+    }
+});
+
+//************************************************************ fonctions
+
+div =function(label,elmt,plus){
+
+    var ctrl="";
+
+    switch(elmt){
+
+        //une réponse
+        case "1":{
+        ctrl="<input  class='form-control'  type='text'   placeholder='"+plus+"' />";
+            break;
+        }
+        //un message
+        case "2":{
+        ctrl="<textarea  class='form-control'  type='text'  placeholder='"+plus+"'><textarea/>";
+            break;
+        }
+        //une date et une heure
+        case "3":{
+            ctrl="<input type='text' class='form-control datepicker' placeholder='"+plus+"' >";
+            break;
+        }
+        case "4":{
+
+
+            break;
+        }
+    }
+    var html="<div class='form-group text-center'><label for='' class='control-label'>"+label+"</label>"+ctrl+"</div>";
+
+    return html;
+}
+
+resetFormGenere = function(){
+    $("#inputTextGenForm").val("");
+    $("#inputSelectGenForm").val("1");
+}
---
+++ collections/formulaires.js
@@ -0,0 +1,28 @@
+/**
+ * Created by lp on 06/04/2016.
+ */
+
+formulaires = new Mongo.Collection("formulaires");
+
+Globals.schemas.Formulaire = new SimpleSchema({
+    nom: {
+        type: String,
+        max: 50,
+        label: "Nom du formulaire"
+    },
+    elements_labels :{
+        type:[String],
+        label:"labels"
+    },
+    elements_types :{
+        type:[String],
+        label:"types"
+    },
+    elements_caracteristiques :{
+        type:[String],
+        label:"caractéristiques"
+    }
+
+});
+
+formulaires.attachSchema(Globals.schemas.Formulaire);
