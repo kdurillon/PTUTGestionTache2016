a3e48ec55ef5c6dd999d529c752c82014562bc5a
Nacim Goura <Nacim Goura>
2016-04-06 11:05:03 +0200

    suppression catégorie

--- client/views/categorie/categories.js
+++ client/views/categorie/categories.js
@@ -4,4 +4,16 @@ Template.categoriesHome.helpers({
    }
 });
 
+Template.categoriesHome.rendered = function() {
+    console.log(categories.find().fetch());
+};
+
+Template.categoriesHome.events({
+    'click .modif_categorie': function() {
+        console.log(this);
+    },
+    'click .delete_categorie': function() {
+        categories.remove(this._id);
+    }
+});
 
